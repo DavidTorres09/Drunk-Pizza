c989d18c32434d1b4f459a370bc95c06870df279
DavidTorres09 <angeldtm09@gmail.com>
2022-11-05 12:20:30 -0600

    Plantilla y conexiones

---
+++ src/main/java/com/DrunkPizza/Drunk/DrunkPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.DrunkPizza.Drunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunkPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrunkPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DrunkPizza/Drunk/DrunkPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DrunkPizza.Drunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrunkPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
