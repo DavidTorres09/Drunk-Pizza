aaad674dad0db5647fc00173fc81a8a8a983753f
DavidTorres09 <angeldtm09@gmail.com>
2022-11-08 16:11:36 -0600

    Commit folders

---
+++ src/main/java/com/DrunkPizza/Drunk/entity/Producto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.DrunkPizza.Drunk.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author angel
+ */
+@Entity
+@Table(name="producto")
+public class Producto {
+    
+}
