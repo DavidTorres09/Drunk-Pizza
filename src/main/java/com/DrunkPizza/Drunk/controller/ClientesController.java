3434cf9b528476b61e3aa9789c66f49f280a4179
Sofiamdrgl <113658408+Sofiamdrgl@users.noreply.github.com>
2022-11-10 23:27:51 -0600

    FInalizacion CRUD de clientes
    Segunda entrega proyecto

---
+++ src/main/java/com/DrunkPizza/Drunk/controller/ClientesController.java
@@ -0,0 +1,56 @@
+
+package com.DrunkPizza.Drunk.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ClientesController {
+    
+    @Autowired
+    private IClienteService clienteService;
+
+
+
+    @GetMapping("/cliente")
+    public String index(Model model) {
+        List<Clientes> listaClientes = clienteService.getAllClientes();
+        model.addAttribute("titulo", "Cliente");
+        model.addAttribute("cliente", listaClientes);
+        return "cliente";
+    }
+
+    @GetMapping("clienteNuevo")
+    public String crearCliente(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "crear";
+    }
+    
+    
+    @PostMapping("/save")
+    public String guardarPersona(@ModelAttribute Cliente cliente){
+        productoService.saveCliente(cliente);
+        return "redirect:/producto";
+    }
+    
+    @GetMapping("/editPersona/{id}")
+    public String editarPersona(@PathVariable("id")Long idCliente, Model model){
+        Cliente cliente = clienteService.getClienteById(idCliente);
+        model.addAttribute("cliente", cliente);
+        return "crear";
+    }
+    
+    @GetMapping("/deleteCliente/{id}")
+    public String eliminarPersona(@PathVariable("id") Long idCliente){
+        clienteService.delete(idCliente);
+        return "redirect:/cliente";
+    }
+    
+}
+
