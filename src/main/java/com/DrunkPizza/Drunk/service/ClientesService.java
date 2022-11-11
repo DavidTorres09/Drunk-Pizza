28676f0abf27961b48be5e94f2359b3b178ea167
Alonsoh301 <alonso.hernandez301@gmail.com>
2022-11-10 23:49:28 -0600

    se agrega mas css y un ico
    las fotos ico son para que en la ventana de la pagina salga un mini logo en ella arriba, tambien corregi errores menores
    
    Co-Authored-By: Sofiamdrgl <113658408+Sofiamdrgl@users.noreply.github.com>

--- src/main/java/com/DrunkPizza/Drunk/service/ClientesService.java
+++ src/main/java/com/DrunkPizza/Drunk/service/ClientesService.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.DrunkPizza.Drunk.service;
-
 import com.DrunkPizza.Drunk.entity.clientes;
 import com.DrunkPizza.Drunk.repository.ClientesRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 /**
  *
  * @author angel
